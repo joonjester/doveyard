83fc1c5741e09d96f89ecec2a8e0d7b972797578
joonjester <hyunjoonchoi04@gmail.com>
2026-03-19 21:24:22 +0100

    refactor(tui): cleaner version

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -9,13 +9,20 @@ import (
 	"charm.land/lipgloss/v2"
 )
 
+type ViewType string
+
+const (
+	horizontalSplit ViewType = "horizontalSplit"
+	verticalSplit   ViewType = "verticalSplit"
+)
+
 type email struct {
-	sender     string
-	receiver   string
-	subject    string
-	content    string
-	time       time.Time
-	readStatus bool
+	sender   string
+	receiver string
+	subject  string
+	content  string
+	time     time.Time
+	hasRead  bool
 }
 
 type size struct {
@@ -24,30 +31,36 @@ type size struct {
 }
 
 type model struct {
-	emails   []email
-	cursor   int
-	selected map[int]struct{}
-	size     size
+	emails    map[int]email
+	cursor    int
+	selected  map[int]struct{}
+	size      size
+	showEmail bool
+	viewType  ViewType
 }
 
 func initialModal() model {
 	return model{
-		emails: []email{{
-			sender:     "test@test.de",
-			receiver:   "test2@test.de",
-			subject:    "This is a test email",
-			content:    "Hello World!",
-			time:       time.Now(),
-			readStatus: false,
-		}, {
-			sender:     "test@test.de",
-			receiver:   "test2@test.de",
-			subject:    "This is a second test email",
-			content:    "Hello World!",
-			time:       time.Now(),
-			readStatus: false,
-		}},
+		emails: map[int]email{
+			0: {
+				sender:   "test@test.de",
+				receiver: "test2@test.de",
+				subject:  "This is a test email",
+				content:  "Hi World!",
+				time:     time.Now(),
+				hasRead:  false,
+			},
+			1: {
+				sender:   "test@test.de",
+				receiver: "test2@test.de",
+				subject:  "This is a second test email",
+				content:  "Hello World!",
+				time:     time.Now(),
+				hasRead:  false,
+			},
+		},
 
+		viewType: horizontalSplit,
 		selected: make(map[int]struct{}),
 	}
 }
@@ -81,10 +94,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 
 		case "enter", "space":
+			email := m.emails[m.cursor]
 			_, ok := m.selected[m.cursor]
 			if ok {
+				email.hasRead = true
 				delete(m.selected, m.cursor)
 			} else {
+				email.hasRead = false
 				m.selected[m.cursor] = struct{}{}
 			}
 		}
@@ -94,43 +110,108 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() tea.View {
-	container := lipgloss.NewStyle().
-		Width(m.size.width).
-		Height(m.size.height).
+	width := m.size.width
+	height := m.size.height
+
+	fullWidth := lipgloss.NewStyle().
 		BorderStyle(lipgloss.RoundedBorder()).
-		BorderTop(true).
-		BorderBottom(true).
-		BorderLeft(true).
-		BorderRight(true)
+		Width(width).
+		Height(height)
 
-	normalStyle := lipgloss.NewStyle()
-	unreadStyle := lipgloss.NewStyle().Foreground(lipgloss.Cyan)
-	selectedStyle := lipgloss.NewStyle().Background(lipgloss.White)
+	emailInbox, openedEmailIndex := allEmails(m)
+	if openedEmailIndex != nil {
+		email := m.emails[*openedEmailIndex]
+		openedEmail := emailContent(email, width)
 
-	var lines string
+		display := splitView(m, emailInbox, openedEmail)
+
+		return tea.NewView(display)
+	}
+
+	return tea.NewView(fullWidth.Render(emailInbox))
+}
+
+func getSplitStyle(isVertical bool, width int, height int) lipgloss.Style {
+	if isVertical {
+		return lipgloss.NewStyle().
+			BorderStyle(lipgloss.RoundedBorder()).
+			Width(width).
+			Height(height / 2)
+	}
+
+	return lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		Width(width / 2).
+		Height(height)
+}
+
+func splitView(m model, emailInbox string, openEmail string) string {
+	isVertical := m.viewType == verticalSplit
+	viewStyleRender := getSplitStyle(isVertical, m.size.width, m.size.height)
+
+	emailInboxRender := viewStyleRender.Render(emailInbox)
+	emailContentRender := viewStyleRender.Render(openEmail)
+
+	if isVertical {
+		return lipgloss.JoinVertical(lipgloss.Left, emailInboxRender, emailContentRender)
+	}
+	return lipgloss.JoinHorizontal(lipgloss.Left, emailInboxRender, emailContentRender)
+}
+
+func emailContent(email email, width int) string {
+	emailAddressStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#808080"))
+	subjectStyle := lipgloss.NewStyle().
+		Bold(true).
+		BorderStyle(lipgloss.Border{Bottom: "="}).
+		BorderBottom(true).Width((width / 2) - 2)
+
+	var emailContent string
+
+	addresses := fmt.Sprintf("From: %s\nTo: %s\n", email.sender, email.receiver)
+	emailContent += fmt.Sprintf(
+		"%s\n%s\n%s",
+		emailAddressStyle.Render(addresses),
+		subjectStyle.Render(email.subject),
+		email.content,
+	)
+
+	return emailContent
+}
+
+func allEmails(m model) (string, *int) {
+	unreadEmail := lipgloss.NewStyle().Foreground(lipgloss.Cyan)
+	hoveringEmail := lipgloss.NewStyle().Background(lipgloss.White)
+	defaultEmail := lipgloss.NewStyle()
+
+	var emails string
+	var openedEmailIndex *int
 	for i, email := range m.emails {
-		line := fmt.Sprintf("[%s] %s\n %s", email.time.Format("02.01.2006"), email.sender, email.subject)
+		message := fmt.Sprintf("[%s] %s\n %s",
+			email.time.Format("02.01.2006"),
+			email.sender,
+			email.subject)
 
 		_, ok := m.selected[i]
-		if ok && email.readStatus == false {
-			email.readStatus = true
+		if ok {
+			openedEmailIndex = &i
 		}
 
-		styledLines := normalStyle.Render(line)
-		if email.readStatus == false {
-			styledLines = unreadStyle.Render(line)
+		if !email.hasRead && !ok {
+			message = unreadEmail.Render(message)
+		} else {
+			message = defaultEmail.Render(message)
 		}
 
 		if m.cursor == i {
-			lines += selectedStyle.Render(styledLines) + "\n"
+			message = hoveringEmail.Render(message) + "\n"
 		} else {
-			lines += styledLines + "\n"
+			message = defaultEmail.Render(message) + "\n"
 		}
+
+		emails += message
 	}
 
-	lines += "\nPress q to quit.\n"
-
-	return tea.NewView(container.Render(lines))
+	return emails, openedEmailIndex
 }
 
 func Run() {
